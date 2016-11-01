d047f8f49a58b45864864974dc5433a699c1ec64
Elham <elham@localhost>
2016-10-31 21:46:10 -0400

    “First”

---
+++ app/src/main/java/com/example/heem/togetherfit/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.heem.togetherfit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    //Take the student to log in screen
+    public void studentLogin(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, LoginActivityStudent.class);
+        startActivity(intent);
+    }
+
+}
