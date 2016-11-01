6a99a570f703100f0cb3b9019213b60b81204339
Elham <elham@localhost>
2016-11-01 18:08:38 -0400

    Third

--- app/src/main/java/com/example/heem/togetherfit/StudentLogin.java
+++ app/src/main/java/com/example/heem/togetherfit/StudentLogin.java
@@ -1,8 +1,13 @@
 package com.example.heem.togetherfit;
 
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class StudentLogin extends AppCompatActivity {
@@ -11,7 +16,17 @@ public class StudentLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
-        TextView text = (TextView) findViewById(R.id.welcomeStudent);
-        text.setTextColor(Color.parseColor("#AFD3DF"));
+
+        //Changes the color
+        TextView welcome = (TextView) findViewById(R.id.welcomeStudent);
+        welcome.setTextColor(Color.parseColor("lightGray"));
+        EditText studentUserName = (EditText) findViewById(R.id.userNameStudent);
+        studentUserName.setBackgroundColor(Color.parseColor("#AFD3DF"));
+        EditText studentPass = (EditText) findViewById(R.id.passStudent);
+        studentPass.setBackgroundColor(Color.parseColor("#AFD3DF"));
+        TextView registerLinkStudent = (TextView) findViewById(R.id.registerLinkStudent);
+        registerLinkStudent.setTextColor(Color.parseColor("#AFD3DF"));
+
+
     }
 }
--- app/src/main/java/com/example/heem/togetherfit/TrainerLogin.java
+++ app/src/main/java/com/example/heem/togetherfit/TrainerLogin.java
@@ -1,7 +1,10 @@
 package com.example.heem.togetherfit;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TrainerLogin extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class TrainerLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainer_login);
+
+        //Changes the color
+        TextView welcome = (TextView) findViewById(R.id.welcomeTrainer);
+        welcome.setTextColor(Color.parseColor("lightGray"));
+        EditText trainerUserName = (EditText) findViewById(R.id.userNameTrainer);
+        trainerUserName.setBackgroundColor(Color.parseColor("#AFD3DF"));
+        EditText trainerPass = (EditText) findViewById(R.id.passTrainer);
+        trainerPass.setBackgroundColor(Color.parseColor("#AFD3DF"));
+        TextView registerLinkTrainer = (TextView) findViewById(R.id.registerLinkTrainer);
+        registerLinkTrainer.setTextColor(Color.parseColor("#AFD3DF"));
     }
 }
