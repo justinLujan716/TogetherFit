d74e82cc798ad3a6d0fd42621603efc7432416ce
Jingyu Wang <jw652401@example.com>
2016-11-07 18:42:54 -0500

    for the group

--- app/src/main/java/com/example/heem/togetherfit/MainActivity.java
+++ app/src/main/java/com/example/heem/togetherfit/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.Button;
+//for the push
 
 
 public class MainActivity extends AppCompatActivity {
