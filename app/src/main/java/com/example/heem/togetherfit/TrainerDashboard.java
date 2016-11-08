b6fd5c05986dff4dcf8782827c81c328c200aee4
Elham <elham@localhost>
2016-11-08 00:54:49 -0500

    TrainerDashboard

--- app/src/main/java/com/example/heem/togetherfit/TrainerDashboard.java
+++ app/src/main/java/com/example/heem/togetherfit/TrainerDashboard.java
@@ -1,13 +1,30 @@
 package com.example.heem.togetherfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrainerDashboard extends AppCompatActivity {
+
+    //Variables
+    Button findplacebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainer_dashboard);
+
+        findplacebtn = (Button) findViewById(R.id.locationtrainerbutton);
+        findplacebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // the name of the receiving activity is declared in the Intent Constructor
+                Intent intent = new Intent(TrainerDashboard.this, FindPlaceTrainer.class);
+                //start the activity
+                startActivity(intent);
+            }
+        });
     }
 }
