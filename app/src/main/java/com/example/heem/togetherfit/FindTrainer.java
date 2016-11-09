bbb86214d5cc66feb00fdb34d3abdd64cd1126bf
Nick Boyd <nb619430@sju.edu>
2016-11-09 11:32:09 -0800

    FindTrainerVersion1

---
+++ app/src/main/java/com/example/heem/togetherfit/FindTrainer.java
@@ -0,0 +1,13 @@
+package com.example.heem.togetherfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindTrainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_trainer);
+    }
+}
