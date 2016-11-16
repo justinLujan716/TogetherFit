a2952e19f011291282492c0409ef2d5ebedc9c44
Jingyu Wang <jw652401@example.com>
2016-11-16 18:59:23 -0500

    Add new requirement, distinction

--- app/src/main/java/com/example/heem/togetherfit/TrainerLogin.java
+++ app/src/main/java/com/example/heem/togetherfit/TrainerLogin.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,15 +15,24 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class TrainerLogin extends AppCompatActivity {
     private EditText inputEmail, inputPassword;
     private FirebaseAuth auth;
     private Button btnSignup, btnLogin;
+    private DatabaseReference data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,15 +93,33 @@ public class TrainerLogin extends AppCompatActivity {
                                         Toast.makeText(TrainerLogin.this, "log in filed", Toast.LENGTH_LONG).show();
                                     }
                                 } else {
-                                    Intent intent = new Intent(TrainerLogin.this, TrainerDashboard.class);
-                                    startActivity(intent);
-                                    finish();
+                                    String Tuid = auth.getCurrentUser().getUid().trim();
+                                    checkType(Tuid);//Check the login use's type
                                 }
                             }
                         });
             }
         });
+    }
+    public void checkType(final String Tuid){
+        DatabaseReference database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://togetherfit-148901.firebaseio.com/User");
+        DatabaseReference databaseRef = database.child(Tuid).child("Type");
 
-
+        // Attach a listener to read the data at our posts reference
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String CurrentType = (String)snapshot.getValue();
+                if (CurrentType.equals("trainer")){
+                    startActivity(new Intent(TrainerLogin.this, TrainerDashboard.class));}
+                else
+                    Toast.makeText(TrainerLogin.this, "You are not a trainer, take you to student dashboard", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(TrainerLogin.this, StudentDashboard.class));
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseErr) {
+                System.out.println("The read failed: " + databaseErr.getCode());
+            }
+        });
     }
 }
