a48f70bea8fa8356960ab40fb70e29282f0d9ed0
Elham <elham@localhost>
2016-11-26 02:01:54 -0500

    Class Registeration Version 2

--- app/src/main/java/com/example/heem/togetherfit/AddClass.java
+++ app/src/main/java/com/example/heem/togetherfit/AddClass.java
@@ -1,9 +1,11 @@
 package com.example.heem.togetherfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,19 +28,14 @@ public class AddClass extends AppCompatActivity {
     ArrayList<String> title = new ArrayList<String>();
     ArrayList<String> type = new ArrayList<String>();
     ArrayList<String> location = new ArrayList<String>();
+    ArrayList<String> image = new ArrayList<>();
+    View mylayout;
+    TextView ClassName;
     //Firebase variable
     DatabaseReference database;
+    //Class Id
+    ArrayList<String> ClassId = new ArrayList<>();
 
-
-    Integer[] imageId = {
-            R.drawable.gym,
-            R.drawable.gym,
-            R.drawable.gym,
-            R.drawable.gym,
-            R.drawable.gym,
-            R.drawable.gym,
-            R.drawable.gym
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +43,15 @@ public class AddClass extends AppCompatActivity {
         setContentView(R.layout.activity_add_class);
         // Connect to the Firebase database and access CreatedClass database
         database = FirebaseDatabase.getInstance().getReference().child("CreatedClass");
-
+        mylayout = findViewById(R.id.details);
+        ClassName = (TextView) findViewById(R.id.ClassName);
         // Attach a listener to read the data at our posts reference
         database.addChildEventListener(new ChildEventListener() {
 
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
+                ClassId.add(dataSnapshot.getRef().getKey());
                 getUpdate(dataSnapshot);
             }
 
@@ -77,9 +76,32 @@ public class AddClass extends AppCompatActivity {
             }
         });
 
+        //Back button takes back to sign in activity
+        //This is the way to refer to outside button from another laytout back button is in header.xml
+        View myLayout = findViewById(R.id.backbtnlayout); // root View id from that link
+        Button backbutton = (Button) myLayout.findViewById(R.id.backbtn); // id of a view contained in the included file
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // the name of the receiving activity is declared in the Intent Constructor, go back to log in page
+                Intent back = new Intent(AddClass.this, StudentDashboard.class);
+                //start the activity
+                startActivity(back);
+            }
+        });
+        //Log out button at the tool bar to take the user to main page
+        View myLayout2 = findViewById(R.id.signOut); // root View id from that link
+        Button signOut = (Button) myLayout2.findViewById(R.id.signOutbtn); // id of a view contained in the included file
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
-      private void getUpdate(DataSnapshot ds)
+    private void getUpdate(DataSnapshot ds)
     {
         //Get the class name and send that to the adapter
         String titleValue = (ds.child("ClassName").getValue()).toString();
@@ -89,30 +111,35 @@ public class AddClass extends AppCompatActivity {
         type.add(typeValue);
         //Get teh class location and send that to the adapter
         String locationValue = (ds.child("TrainPlace").getValue()).toString();
+        //Get the image URL
+        String imageURL = (ds.child("ImageURL").getValue()).toString();
+        image.add(imageURL);
         location.add(locationValue);
         if (title.size() >0)
         {
+
             CustomList adapter = new
-                    CustomList(AddClass.this, title, imageId, type,location);
+                    CustomList(AddClass.this, title, image, type,location);
             list=(ListView)findViewById(R.id.listAll);
             list.setAdapter(adapter);
+            //perform a specific action when the user click on a class redirect the user to class's detiales
             list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
                 //When they click on an Item
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Toast.makeText(AddClass.this, "You Clicked at " + title.get(+position) + "\nI am taking you to the details page", Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(AddClass.this, "You Clicked at " + title.get(+position) + "\nI took you to class's details", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AddClass.this,ClassDetail.class);
+                    intent.putExtra("Value", ClassId.get(+position));
+                    startActivity(intent);
 
                 }
             });
-
         }
         else
         {
             Toast.makeText(AddClass.this, "No data to view", Toast.LENGTH_SHORT).show();
+
         }
     }
-
-
 }
--- app/src/main/java/com/example/heem/togetherfit/CustomList.java
+++ app/src/main/java/com/example/heem/togetherfit/CustomList.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
@@ -17,12 +19,12 @@ public class CustomList extends ArrayAdapter<String>{
 
     private final Activity context;
     private final ArrayList<String> title;
-    private final Integer[] imageId;
+    private final ArrayList<String> imageId;
     private final ArrayList<String> type;
     private final ArrayList<String> location;
 
     public CustomList(Activity context,
-                      ArrayList<String> title, Integer[] imageId, ArrayList<String> type, ArrayList<String> location) {
+                      ArrayList<String> title, ArrayList<String> imageId, ArrayList<String> type, ArrayList<String> location) {
         super(context, R.layout.activity_add_class, title);
         this.context = context;
         this.title = title;
@@ -40,7 +42,8 @@ public class CustomList extends ArrayAdapter<String>{
         TextView txtType = (TextView) rowView.findViewById(R.id.type);
         TextView txtLocation = (TextView) rowView.findViewById(R.id.location);
         txtTitle.setText(title.get(position));
-        imageView.setImageResource(imageId[position]);
+        //Use Picasso Jar file to display the image
+        Picasso.with(context).load(imageId.get(position)).into(imageView);
         txtType.setText("Fitness Type: " + type.get(position));
         txtLocation.setText("Class Location: " + location.get(position));
         return rowView;
